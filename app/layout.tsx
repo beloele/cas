337bb099ebe99482bec3d79d28c5acc9420c2a62
belo <69517797+beloele@users.noreply.github.com>
2024-07-26 17:23:29 +0300

    dfgd
    fgdf

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,6 @@ export const metadata: Metadata = {
   keywords: "Casibom, canlı bahis, spor bahisleri, casino, yüksek oranlar, bonus, futbol bahisleri, basketbol bahisleri, tenis bahisleri",
   authors: [{ name: "Casibom", url: "https://casibom.vercel.app" }],
   viewport: "width=device-width, initial-scale=1.0",
-  charset: "UTF-8",
   robots: "index, follow",
   canonical: "https://casibom.vercel.app",
   og: {
