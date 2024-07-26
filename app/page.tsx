7814de7694e26c661125bb2e5b40f073fe3a60ff
belo <69517797+beloele@users.noreply.github.com>
2024-07-26 18:50:10 +0300

    g
    g

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from "react";
 import Image from "next/image";
 import Link from 'next/link';
-import { Analytics } from "@vercel/analytics/react"
+
 
 export default function Home() {
   return (
