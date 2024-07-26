a5d02e09247510806808018743e46674c668ae12
belo <69517797+beloele@users.noreply.github.com>
2024-07-26 17:43:29 +0300

    sfgs
    fg

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,23 @@ export default function Home() {
             <Image
               className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
               src="/logo.png"
-              alt="Casibom logo"
-              width={500}
-              height={500}
+              alt="Casibom Guncel Giris Adresi"
+              width={350}
+              height={350}
+              priority
+            />
+          </a>
+        </Link>
+      </div>
+      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] mb-8">
+        <Link href="#" passHref>
+          <a>
+            <Image
+              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+              src="/giris.png"
+              alt="Casibom Giriş"
+              width={350}
+              height={200}
               priority
             />
           </a>
@@ -25,18 +39,7 @@ export default function Home() {
       <div className="mt-8" style={{
         marginTop: 40
       }}>
-      <Link href="#" passHref>
-          <a>
-            <Image
-              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-              src="/giris.png"
-              alt="Casibom logo"
-              width={500}
-              height={200}
-              priority
-            />
-          </a>
-        </Link>
+      
         <Link href="#" passHref>
           <a className="inline-block px-8 py-4 bg-[#ff6e01] text-white font-semibold rounded-md transition-transform duration-300 transform hover:scale-105 hover:bg-[#4891ff]">
             Casibom Giriş İçin Tıkla
